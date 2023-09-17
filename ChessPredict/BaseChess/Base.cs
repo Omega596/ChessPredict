acc272e1f2fe61abff138ab9776ec2a9a23faae2
Omega596 <XLfoxs@gmail.com>
2023-09-17 19:26:02 +1000

    Add project files.

---
+++ ChessPredict/BaseChess/Base.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ChessPredict.BaseChess.Base
+
+{
+    public abstract class Predict { }
+    public struct Piece
+    {
+        public AllPieces CurrentPiece = new();
+        public enum AllPieces
+        {
+            None,
+            Pawn,
+            Bishop = 3,
+            Knight,
+            Rook = 5,
+            Queen = 9,
+            King
+        }
+        public enum Color
+        {
+            White,
+            Black
+        }
+        public Piece() { }
+    }
+    public class Board
+    {
+        public Piece[,] BoardMatrixFill()
+        {
+            Piece[,] BoardMatrix = new Piece[8, 8];
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    BoardMatrix[i, j] = new Piece { CurrentPiece = Piece.AllPieces.None };
+                }
+            }
+            return BoardMatrix;
+        }
+        public Piece[,] BoardMatrix = new Piece[8, 8];
+        public Board()
+        {
+            BoardMatrix = BoardMatrixFill();
+
+        }
+        private bool Checkmate;
+        public bool checkmate
+        {
+            get
+            {
+                return Checkmate;
+            }
+            set
+            {
+                if (value == true && Checkmate == false)
+                {
+                    Checkmate = true;
+                }
+                else if (value == false && Checkmate == true && ResetGame == true)
+                {
+                    Checkmate = true;
+                }
+                else
+                {
+                    Console.WriteLine("Tried to write to Checkmate, but conditctional switch denied change.");
+                    return;
+                }
+            }
+        }
+        public bool ResetGame = false;
+    }
+}
---
+++ ChessPredict/BaseChess/GameLogic.cs
@@ -0,0 +1,166 @@
+﻿using SLibrary.features;
+using static ChessPredict.BaseChess.Base.Piece;
+
+namespace ChessPredict.BaseChess.Base.GameLogic
+{
+    class Logic
+    {
+        public static bool IsInBoundsMove(int x, int y)
+        {
+            bool validx;
+            bool validy;
+            validx = x switch
+            {
+                < 0 => false,
+                > 7 => false,
+                _ => true,
+
+            };
+            validy = y switch
+            {
+                < 0 => false,
+                > 7 => false,
+                _ => true,
+            };
+            return validx && validy;
+        }
+        public static List<(int, int)> GetLegalMoves(AllPieces piece, int x, int y)
+        {
+            List<(int, int)> legalMoves = new();
+
+            switch (piece)
+            {
+                case AllPieces.None: break;
+                case AllPieces.Bishop:
+                {
+                    List<(int, int)> directions = new() { (-1,1), (1,1), (-1,-1), (1,-1) };
+                    foreach ((int, int) direction in directions)
+                    {
+                        for (int i = 0; i < 7; i++)
+                        {
+                            int nx = x + direction.Item1;
+                            int ny = y + direction.Item2;
+                            (int, int) position = (nx, ny);
+                            if (IsInBoundsMove(position.Item1, position.Item2))
+                            {
+                                legalMoves.Add((position.Item1, position.Item2));
+                            }
+                        }
+                    }
+                    return legalMoves;
+                }
+                case AllPieces.Rook:
+                    {
+                        List<(int, int)> directions = new() { (-1, 0), (1, 0), (0, 1), (0, -1) };
+                        foreach ((int, int) direction in directions)
+                        {
+                            for (int i = 0; i < 7; i++)
+                            {
+                                int nx = x + direction.Item1;
+                                int ny = y + direction.Item2;
+                                (int, int) position = (nx, ny);
+                                if (IsInBoundsMove(position.Item1, position.Item2))
+                                {
+                                    legalMoves.Add((position.Item1, position.Item2));
+                                }
+                            }
+                        }
+                        return legalMoves;
+                    }
+                case AllPieces.Pawn:
+                    {
+                        List<(int, int)> directions = new() { (0,1) };
+                        foreach ((int, int) direction in directions)
+                        {
+                            for (int i = 0; i <= 0; i++)
+                            {
+                                int nx = x + direction.Item1;
+                                int ny = y + direction.Item2;
+                                (int, int) position = (nx, ny);
+                                if (IsInBoundsMove(position.Item1, position.Item2))
+                                {
+                                    legalMoves.Add((position.Item1, position.Item2));
+                                }
+                            }
+                        }
+                        return legalMoves;
+                    }
+                case AllPieces.Queen:
+                    {
+                        List<(int, int)> directions = new() { (-1, 1), (0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, -1), (1, -1) };
+                        foreach ((int, int) direction in directions)
+                        {
+                            for (int i = 0; i < 7; i++)
+                            {
+                                int nx = x + direction.Item1;
+                                int ny = y + direction.Item2;
+                                (int, int) position = (nx, ny);
+                                if (IsInBoundsMove(position.Item1, position.Item2))
+                                {
+                                    legalMoves.Add((position.Item1, position.Item2));
+                                }
+                            }
+                        }
+                        return legalMoves;
+                    }
+                case AllPieces.King:
+                    {
+                        List<(int, int)> directions = new() { (-1, 1), (0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, -1), (1, -1) };
+                        foreach ((int, int) direction in directions)
+                        {
+                            for (int i = 0; i <= 0; i++)
+                            {
+                                int nx = x + direction.Item1;
+                                int ny = y + direction.Item2;
+                                (int, int) position = (nx, ny);
+                                if (IsInBoundsMove(position.Item1, position.Item2))
+                                {
+                                    legalMoves.Add((position.Item1, position.Item2));
+                                }
+                            }
+                        }
+                        return legalMoves;
+                    }
+                case AllPieces.Knight:
+                    {
+                        List<(int, int)> directions = new() { (-1, 2), (1, 2), (-2, 1), (-2, -1), (2, 1), (2, -1), (-1, -2), (1, -2) };
+                        foreach ((int, int) direction in directions)
+                        {
+                            for (int i = 0; i <= 0; i++)
+                            {
+                                int nx = x + direction.Item1;
+                                int ny = y + direction.Item2;
+                                (int, int) position = (nx, ny);
+                                if (IsInBoundsMove(position.Item1, position.Item2))
+                                {
+                                    legalMoves.Add((position.Item1, position.Item2));
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                        return legalMoves;
+                    }
+            }
+            return legalMoves;
+        }
+    }
+    class Init
+    {
+        static void Main()
+        {
+            Board board = new();
+            List<(int, int)> legalMoves = new List<(int, int)>(Logic.GetLegalMoves(AllPieces.Knight, 5, 2));
+            for (int i = 0; i < legalMoves.Count; i++)
+            {
+                Print.Printl(legalMoves[i]);
+            }
+        }
+        public void Initialization()
+        {
+            // Code for initialization goes here
+        }
+    }
+}
